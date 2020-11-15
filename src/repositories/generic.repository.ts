e3aeac72b044bb77c99a000c4456258c3beb6031
Naylepsh <naylepsh@gmail.com>
2020-11-15 15:12:23 +0100

    add basic test on find with params

--- src/repositories/generic.repository.ts
+++ src/repositories/generic.repository.ts
@@ -1,12 +1,23 @@
 import { AbstractRepository, EntityRepository } from "typeorm";
 import { IRepository } from "./repository.interface";
 
+export interface FindParams {
+  where?: any;
+  take?: number;
+  skip?: number;
+  relations?: string[];
+}
+
 @EntityRepository()
 export class GenericRepository<Entity>
   extends AbstractRepository<Entity>
   implements IRepository<Entity> {
   save(entity: Entity): Promise<Entity> {
     return this.repository.save(entity);
+  }
+
+  findMany(findParams?: FindParams): Promise<Entity[]> {
+    return this.repository.find(findParams);
   }
 
   findById(id: number, relations: string[] = []): Promise<Entity | undefined> {
