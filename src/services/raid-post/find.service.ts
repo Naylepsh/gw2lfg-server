31f3741b7d7ee8b1a68c3b56fbe9435fef06d380
Naylepsh <naylepsh@gmail.com>
2020-11-26 16:17:35 +0100

    turn find raid post into serive class

--- src/services/raid-post/find.service.ts
+++ src/services/raid-post/find.service.ts
@@ -1,9 +1,15 @@
 import { IRaidPostRepository } from "../../repositories/raid-post.repository";
 
-export const find = async (
-  skip: number | undefined,
-  take: number | undefined,
-  repository: IRaidPostRepository
-) => {
-  return repository.findMany({ order: { date: "DESC" }, skip, take });
-};
+export interface FindRaidPostParams {
+  skip?: number;
+  take?: number;
+}
+
+export class FindRaidPostService {
+  constructor(private readonly repository: IRaidPostRepository) {}
+
+  find(params: FindRaidPostParams) {
+    const { skip, take } = params;
+    return this.repository.findMany({ order: { date: "DESC" }, skip, take });
+  }
+}
