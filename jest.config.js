e222c3bd28e2260be76cd683f93c12919d378f11
Naylepsh <naylepsh@gmail.com>
2020-12-18 17:47:40 +0100

    move test folder

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  roots: ["<rootDir>/src/test/"],
+  roots: ["<rootDir>/test/"],
   testMatch: [
     "**/__tests__/**/*.+(ts|tsx|js)",
     "**/?(*.)+(spec|test).+(ts|tsx|js)",
