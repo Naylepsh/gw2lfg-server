4c8e8a7c65d4a8d11040b91d0293a30c6521fe5c
Naylepsh <naylepsh@gmail.com>
2021-01-08 17:02:46 +0100

    comment gw2-items service

--- src/services/gw2-items/gw2-items.service.ts
+++ src/services/gw2-items/gw2-items.service.ts
@@ -1,5 +1,8 @@
 import * as gw2items from "./items.json";
 
+/*
+Maps item name to its corresponding item id used by GW2 API
+*/
 export const nameToId = (name: string): string => {
   return (<any>gw2items)[name];
 };
