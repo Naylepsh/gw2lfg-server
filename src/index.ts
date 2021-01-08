0ac8ba06c5b3e6804800bd1cd100a4042f488640
Naylepsh <naylepsh@gmail.com>
2021-01-08 11:37:11 +0100

    add comment to index

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import "./data/repositories/user/user.repository";
 import { loadDependencies } from "./loaders";
 import { config } from "./config";
 
+/*
+The main file responsible for start up of the app
+*/
 const main = async () => {
   const { app } = await loadDependencies();
 
