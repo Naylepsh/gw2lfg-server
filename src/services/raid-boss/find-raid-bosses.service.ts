4fbf8f121a35213db3ac3ec5e433b3374636b336
Naylepsh <naylepsh@gmail.com>
2021-01-08 17:22:41 +0100

    comment raid bosses services

--- src/services/raid-boss/find-raid-bosses.service.ts
+++ src/services/raid-boss/find-raid-bosses.service.ts
@@ -2,6 +2,10 @@ import { Inject, Service } from "typedi";
 import { raidBossRepositoryType } from "@loaders/typedi.constants";
 import { IRaidBossRepository } from "@data/repositories/raid-boss/raid-boss.repository.interface";
 
+/*
+Service for finding all raid bosses.
+Sorts results by id.
+*/
 @Service()
 export class FindRaidBossesService {
   constructor(
--- src/services/raid-boss/index.ts
+++ src/services/raid-boss/index.ts
@@ -1,5 +1,6 @@
 import { FindRaidBossesService } from "./find-raid-bosses.service";
 
+// Available services
 export default {
   FindRaidBossesService,
 };
