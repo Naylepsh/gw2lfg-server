db1b3f33691b23bdfeb814ba7a660d0d49f011b5
Naylepsh <naylepsh@gmail.com>
2021-01-10 13:48:51 +0100

    make name to id conversion more error proof

--- src/services/gw2-items/gw2-items.service.ts
+++ src/services/gw2-items/gw2-items.service.ts
@@ -1,8 +1,9 @@
-import * as gw2items from "./items.json";
+import gw2items from "./items.json";
 
 /*
 Maps item name to its corresponding item id used by GW2 API
 */
 export const nameToId = (name: string): number => {
-  return parseInt((<any>gw2items)[name]);
+  const defaultId = -1; // no item has this id, but it prevents more unexpected errors
+  return (<Record<string, number>>gw2items)[name] ?? defaultId;
 };
