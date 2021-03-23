6d3dd4f81cbf4fc0af4f0073f589a9ab6ebe1b7b
Naylepsh <naylepsh@gmail.com>
2021-03-23 14:12:41 +0100

    partition deleteOldPosts into multiple smaller functions

--- src/services/raid-post/delete-old-posts.service.ts
+++ src/services/raid-post/delete-old-posts.service.ts
@@ -1,5 +1,6 @@
 import { Inject, Service } from "typedi";
 import { IRaidPostUnitOfWork } from "@data/units-of-work/raid-post/raid-post.unit-of-work.interface";
+import { RaidPost } from "@data/entities/raid-post/raid-post.entitity";
 import { raidPostUnitOfWorkType } from "@loaders/typedi.constants";
 import { In, LessThanOrEqual } from "typeorm";
 
@@ -14,45 +15,60 @@ export class DeleteOldPostsService {
 
   async deleteOldPosts() {
     return this.uow.withTransaction(async () => {
-      // find all posts such that their date < now
-      const posts = await this.uow.raidPosts.findMany({
-        where: { date: LessThanOrEqual(new Date()) },
-        relations: ["requirements", "roles"],
-      });
-      const postsIds = posts.map((post) => post.id);
+      const posts = await this.getOldPosts();
 
-      // find and remove all of their join requests
-      if (posts.length > 0) {
-        const joinRequests = await this.uow.joinRequests.findMany({
-          where: { post: { id: In(postsIds) } },
-          relations: ["post"],
-        });
-        const joinRequestsIds = joinRequests.map((request) => request.id);
-        if (joinRequestsIds.length > 0) {
-          await this.uow.joinRequests.delete(joinRequestsIds);
-        }
-      }
+      await this.removeJoinRequestsToPosts(posts);
+      await this.removeRequirementsOfPosts(posts);
+      await this.removeRolesOfPosts(posts);
 
-      // find and remove all of their requirements
-      const requirements = posts.map((post) => post.requirements).flat();
-      const requirementsIds = requirements.map((requirement) => requirement.id);
-      if (requirementsIds.length > 0) {
-        await this.uow.requirements.delete(requirementsIds);
-      }
-
-      // find and remove all of their roles
-      const roles = posts.map((post) => post.roles).flat();
-      const rolesIds = roles.map((role) => role.id);
-      if (rolesIds.length > 0) {
-        await this.uow.roles.delete(rolesIds);
-      }
-
-      // remove posts
-      if (postsIds.length > 0) {
-        await this.uow.raidPosts.delete(postsIds);
-      }
-
-      return;
+      await this.removePosts(posts);
     });
+  }
+
+  // find all posts such that their date < now
+  private async getOldPosts() {
+    return await this.uow.raidPosts.findMany({
+      where: { date: LessThanOrEqual(new Date()) },
+      relations: ["requirements", "roles"],
+    });
+  }
+
+  // TODO: refactor (just delete in one step instead of finding first)
+  private async removeJoinRequestsToPosts(posts: RaidPost[]) {
+    if (posts.length > 0) {
+      const postsIds = posts.map((post) => post.id);
+      const joinRequests = await this.uow.joinRequests.findMany({
+        where: { post: { id: In(postsIds) } },
+        relations: ["post"],
+      });
+
+      const joinRequestsIds = joinRequests.map((request) => request.id);
+      if (joinRequestsIds.length > 0) {
+        await this.uow.joinRequests.delete(joinRequestsIds);
+      }
+    }
+  }
+
+  private async removeRequirementsOfPosts(posts: RaidPost[]) {
+    const requirements = posts.map((post) => post.requirements).flat();
+    const requirementsIds = requirements.map((requirement) => requirement.id);
+    if (requirementsIds.length > 0) {
+      await this.uow.requirements.delete(requirementsIds);
+    }
+  }
+
+  private async removeRolesOfPosts(posts: RaidPost[]) {
+    const roles = posts.map((post) => post.roles).flat();
+    const rolesIds = roles.map((role) => role.id);
+    if (rolesIds.length > 0) {
+      await this.uow.roles.delete(rolesIds);
+    }
+  }
+
+  private async removePosts(posts: RaidPost[]) {
+    const postsIds = posts.map((post) => post.id);
+    if (postsIds.length > 0) {
+      await this.uow.raidPosts.delete(postsIds);
+    }
   }
 }
