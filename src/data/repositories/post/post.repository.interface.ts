9f3864f04ed33cd6320d180e1540a47f384f9693
Naylepsh <naylepsh@gmail.com>
2021-04-02 14:22:53 +0200

    refactor post repo

--- src/data/repositories/post/post.repository.interface.ts
+++ src/data/repositories/post/post.repository.interface.ts
@@ -1,4 +1,37 @@
 import { Post } from "../../entities/post/post.entity";
-import { IIdentifiableEntityRepository } from "../identifiable-entity.repository.interface";
 
-export interface IPostRepository extends IIdentifiableEntityRepository<Post> {}
+export interface IPostRepository {
+  save(post: Post): Promise<Post>;
+  findOne(params: PostQueryParams): Promise<Post | undefined>;
+  findMany(params: PostsQueryParams): Promise<Post[]>;
+  delete(criteria?: any): Promise<void>;
+}
+
+export interface PostQueryParams {
+  whereParams?: PostWhereParams;
+}
+
+export interface PostsQueryParams extends PostQueryParams {
+  skip?: number;
+  take?: number;
+}
+
+export interface PostWhereParams {
+  id?: number;
+  minDate?: string;
+  server?: string;
+  author?: PostWhereAuthorParams;
+  role?: PostWhereRoleParams;
+}
+
+export interface PostWhereAuthorParams {
+  id?: number;
+  name?: string;
+}
+
+export interface PostWhereRoleParams {
+  name?: string;
+  eitherName?: [string, string];
+  class?: string;
+  eitherClass?: [string, string];
+}
--- src/data/repositories/post/post.repository.ts
+++ src/data/repositories/post/post.repository.ts
@@ -1,17 +1,148 @@
 import { Service } from "typedi";
-import { EntityRepository } from "typeorm";
+import { AbstractRepository, EntityRepository, Like, MoreThan } from "typeorm";
 import { Post } from "../../entities/post/post.entity";
-import { IdentifiableEntityRepository } from "../generic-identifiable-entity.repository";
-import { IPostRepository } from "./post.repository.interface";
+import {
+  IPostRepository,
+  PostQueryParams,
+  PostsQueryParams,
+  PostWhereParams,
+} from "./post.repository.interface";
 
 @Service()
 @EntityRepository(Post)
 export class PostRepository
-  extends IdentifiableEntityRepository<Post>
+  extends AbstractRepository<Post>
   implements IPostRepository {
-  private static relations = ["author", "requirements", "roles"];
+  save(post: Post): Promise<Post> {
+    return this.repository.save(post);
+  }
 
-  findById(id: number): Promise<Post | undefined> {
-    return super.findById(id, PostRepository.relations);
+  findOne(params: PostQueryParams): Promise<Post | undefined> {
+    const { join, where } = parseFindPostsQuery(params);
+    return this.repository.findOne({
+      join,
+      where,
+      relations: PostRepository.relations,
+    });
+  }
+
+  findMany(params: PostsQueryParams): Promise<Post[]> {
+    const { skip, take } = params;
+    const { join, where } = parseFindPostsQuery(params);
+    return this.repository.find({
+      skip,
+      take,
+      join,
+      where,
+      relations: PostRepository.relations,
+    });
+  }
+
+  async delete(criteria: any = {}): Promise<void> {
+    await this.repository.delete(criteria);
+  }
+
+  public static relations = ["author", "requirements", "roles"];
+}
+
+export function parseFindPostsQuery(queryParams: PostQueryParams) {
+  const { whereParams } = queryParams;
+
+  const join = whereParams ? createJoinParams(whereParams) : undefined;
+  const where = whereParams ? createWhereQueryBuilder(whereParams) : undefined;
+
+  return { join, where };
+}
+
+/**
+ * Creates join attributes required for proper query builder functioning
+ */
+function createJoinParams(whereParams: PostWhereParams) {
+  const { author, role } = whereParams;
+  const alias = "post";
+  let join: Record<string, string> = {};
+
+  if (author) {
+    join.author = `${alias}.author`;
+  }
+
+  if (role) {
+    join.roles = `${alias}.roles`;
+  }
+
+  return { alias, innerJoin: join };
+}
+
+function createWhereQueryBuilder(whereParams: PostWhereParams) {
+  return (qb: any) => {
+    addQueryOnRaidPostProps(whereParams, qb);
+    addQueryOnAuthorProps(whereParams, qb);
+    addQueryOnRoleProps(whereParams, qb);
+  };
+}
+
+function addQueryOnRaidPostProps(whereParams: PostWhereParams, qb: any) {
+  const { id, minDate, server } = whereParams;
+
+  if (id) {
+    qb.andWhere({ id });
+  }
+
+  if (minDate) {
+    qb.andWhere({ date: MoreThan(minDate) });
+  }
+
+  if (server) {
+    qb.andWhere({ server: Like(server) });
+  }
+}
+
+function addQueryOnAuthorProps(whereParams: PostWhereParams, qb: any) {
+  const { author } = whereParams;
+  const id = author?.id;
+  const name = author?.name;
+
+  if (id) {
+    qb.andWhere("author.id = :id", { id });
+  }
+
+  if (name) {
+    qb.andWhere("author.username = :name", { name });
+  }
+}
+
+function addQueryOnRoleProps(whereParams: PostWhereParams, qb: any) {
+  const { role } = whereParams;
+
+  if (role?.name) {
+    qb.andWhere("LOWER(roles.name) = LOWER(:roleName)", {
+      roleName: role.name,
+    });
+  }
+
+  if (role?.class) {
+    qb.andWhere("LOWER(roles.class) = LOWER(:roleClass)", {
+      roleClass: role.class,
+    });
+  }
+
+  /**
+   * IMPORTANT!
+   * Conditions that use 'OR' have to be inside parenthesis.
+   * Otherwise we get (c1 AND c2) OR (c3 AND c4) instead of c1 AND (c2 OR c3) AND c4
+   */
+  if (role?.eitherName) {
+    const [role1, role2] = role.eitherName;
+    qb.andWhere(
+      "(LOWER(roles.name) = LOWER(:role1) OR LOWER(roles.name) = LOWER(:role2))",
+      { role1, role2 }
+    );
+  }
+
+  if (role?.class) {
+    qb.andWhere(
+      "(LOWER(roles.class) = LOWER(:roleClass) OR LOWER(roles.class) = 'any')",
+      { roleClass: role.class }
+    );
   }
 }
