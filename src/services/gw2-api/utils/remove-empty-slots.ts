43765e64b70ba55d0d76f6973fc5e0971e7f3e3d
Naylepsh <naylepsh@gmail.com>
2021-05-12 17:23:20 +0200

    cast slot to bool in filter

--- src/services/gw2-api/utils/remove-empty-slots.ts
+++ src/services/gw2-api/utils/remove-empty-slots.ts
@@ -3,5 +3,5 @@
  * If a slot in inventory is empty, GW2 API returns null in its place, thus we remove all the nulls
  */
 export const removeEmptySlots = (slots: any[]) => {
-  return slots.filter((slot) => slot);
+  return slots.filter((slot) => !!slot);
 };
