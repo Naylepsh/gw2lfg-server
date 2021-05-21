1b512fcd8a32688057f11d815939b7a36e13e5bd
Naylepsh <naylepsh@gmail.com>
2021-05-21 14:04:08 +0200

    remove default ids / names from gw2-items

--- src/services/gw2-items/gw2-items.service.ts
+++ src/services/gw2-items/gw2-items.service.ts
@@ -8,14 +8,12 @@ const idToNameMap = Object.fromEntries(idNamePairs);
  * Maps item name to its corresponding item id used by GW2 API
  */
 export const nameToId = (name: string): number => {
-  const defaultId = -1; // no item has this id, but it prevents more unexpected errors
-  return (<Record<string, number>>gw2items)[name] ?? defaultId;
+  return (<Record<string, number>>gw2items)[name];
 };
 
 /**
  * Maps item id to its corresponding item name used by this service
  */
 export const idToName = (id: number): string => {
-  const defaultName = "N/A"; // prevents unexpected errors
-  return idToNameMap[id] ?? defaultName;
+  return idToNameMap[id];
 };
