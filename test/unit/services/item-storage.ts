1ecdc870397b5d02db1a8d9b20649b64e42305c5
Naylepsh <naylepsh@gmail.com>
2021-06-12 18:39:41 +0200

    fix importing moved module issue

--- test/unit/services/item-storage.ts
+++ test/unit/services/item-storage.ts
@@ -1,5 +1,5 @@
 import { GetItems } from "@root/services/gw2-api/items/get-items.fetcher";
-import { GW2ApiItem } from "@services/gw2-items/item.interface";
+import { GW2ApiItem } from "../../services/gw2-api/items/item.interface";
 
 type ItemStorage = Map<string, GW2ApiItem[]>;
 
